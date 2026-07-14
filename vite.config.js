4fddcd68616794a8a6fa2f85eae198fa229f6877
chirag <chirag2gmail.com>
2026-07-14 02:28:58 +0000

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
     port: 5222,
     strictPort: true,
+    allowedHosts: [
+      'tokriii.com',
+      'www.tokriii.com',
+    ],
   },
   preview: {
     host: true,
     port: 5222,
     strictPort: true,
+    allowedHosts: [
+      'tokriii.com',
+      'www.tokriii.com',
+    ],
   },
 })
